fa7f23dcb649e8835f60ff03da6d411bb601165a
Maxougouzou <115991340+Maxougouzou@users.noreply.github.com>
2024-06-17 10:48:30 +0200

    Initial commit

---
+++ app/_layout.js
@@ -0,0 +1,55 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profiles',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Messages',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'chatbubble' : 'chatbubble-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: 'Notifications',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'notifications' : 'notifications-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="servers"
+        options={{
+          title: 'Servers',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'server' : 'server-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
